f9398760ef3ce25ad931ac233cf582498f354eb4
Tyler Wiebe <bibbi8448@gmail.com>
2024-02-05 07:44:50 -0600

    Create IScriptableQueue.cs

---
+++ Runtime/Scripts/Interfaces/IScriptableQueue.T.cs
@@ -0,0 +1,11 @@
+namespace TWI.Collections.Interfaces
+{
+    public interface IScriptableQueue<T> : IScriptableCollection<T>, IScriptableQueue
+    {
+        T Dequeue();
+        void Enqueue(T item);
+        T Peek();
+        bool TryDequeue(out T result);
+        bool TryPeek(out T result);
+    }
+}
---
+++ Runtime/Scripts/Interfaces/IScriptableQueue.cs
@@ -0,0 +1,7 @@
+namespace TWI.Collections.Interfaces
+{
+    public interface IScriptableQueue : IScriptableCollection
+    {
+
+    }
+}
